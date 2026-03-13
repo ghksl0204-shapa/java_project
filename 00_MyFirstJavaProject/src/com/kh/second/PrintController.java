a2daa2878aa79b385e9e85e77ab66340269d06ef
ghksl0204-shapa <ghksl0204-shapa>
2026-03-13 17:33:14 +0900

    수업내용

---
+++ 00_MyFirstJavaProject/src/com/kh/second/PrintController.java
@@ -0,0 +1,9 @@
+package com.kh.second;
+
+public class PrintController {
+
+	public void printMyName() {
+		System.out.println("바보바보바보야");
+	}
+
+}
---
+++ 01_Variable/src/com/kh/run/Run.java
@@ -0,0 +1,16 @@
+package com.kh.run;
+
+import com.kh.variable.Variable;
+// 깃깃깃깃
+
+public class Run {
+	
+	public static void main(String[] args) {
+		
+		Variable v = new Variable();
+		// v.바지를저장하는기능();
+		// v.findLunchMenu();
+		v.declareVariable();
+	}
+
+}
